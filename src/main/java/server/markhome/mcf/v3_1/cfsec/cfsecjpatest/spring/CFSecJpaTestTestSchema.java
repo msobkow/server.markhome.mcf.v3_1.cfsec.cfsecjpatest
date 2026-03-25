2e88fdf3d83d568727d4076aa5d2494ccd4b72e0
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-25 05:00:40 -0600

    2026-03-25 04:45 CST I've realized the real problem I'm having with the code produced is that the Superior relationship classification has to accept any unique index as the ToIndex of the relationship, not just primary indexes. This requires a series of 2.13 engine code changes, and possible knowledge base changes for the buff and jpa modules.

--- src/main/java/server/markhome/mcf/v3_1/cfsec/cfsecjpatest/spring/CFSecJpaTestTestSchema.java
+++ src/main/java/server/markhome/mcf/v3_1/cfsec/cfsecjpatest/spring/CFSecJpaTestTestSchema.java
@@ -75,6 +75,15 @@ public class CFSecJpaTestTestSchema {
 	private CFSecJpaISOTZoneService cFSecISOTZoneService;
 
 	@Autowired
+	private CFSecJpaSecUserService cFSecSecUserService;
+
+	@Autowired
+	private CFSecJpaSecUserPasswordService cFSecSecUserPasswordService;
+
+	@Autowired
+	private CFSecJpaSecUserPWHistoryService cFSecSecUserPWHistoryService;
+
+	@Autowired
 	private CFSecJpaSecSysGrpService cFSecSecSysGrpService;
 
 	@Autowired
@@ -103,15 +112,6 @@ public class CFSecJpaTestTestSchema {
 
 	@Autowired
 	private CFSecJpaSecSessionService cFSecSecSessionService;
-
-	@Autowired
-	private CFSecJpaSecUserService cFSecSecUserService;
-
-	@Autowired
-	private CFSecJpaSecUserPasswordService cFSecSecUserPasswordService;
-
-	@Autowired
-	private CFSecJpaSecUserPWHistoryService cFSecSecUserPWHistoryService;
 
 	@Autowired
 	private CFSecJpaSysClusterService cFSecSysClusterService;
@@ -177,6 +177,30 @@ public class CFSecJpaTestTestSchema {
 		}
 		else {
 			messages.append("Retrieved " + iSOTZoneResults.size() + " entities from CFSec.ISOTZone\n");
+		}
+
+		List<?> secUserResults = cFSecSecUserService.findAll();
+		if (secUserResults == null) {
+			messages.append("Erroneously retrieved null for CFSecSecUserService.findAll()\n");
+		}
+		else {
+			messages.append("Retrieved " + secUserResults.size() + " entities from CFSec.SecUser\n");
+		}
+
+		List<?> secUserPasswordResults = cFSecSecUserPasswordService.findAll();
+		if (secUserPasswordResults == null) {
+			messages.append("Erroneously retrieved null for CFSecSecUserPasswordService.findAll()\n");
+		}
+		else {
+			messages.append("Retrieved " + secUserPasswordResults.size() + " entities from CFSec.SecUserPassword\n");
+		}
+
+		List<?> secUserPWHistoryResults = cFSecSecUserPWHistoryService.findAll();
+		if (secUserPWHistoryResults == null) {
+			messages.append("Erroneously retrieved null for CFSecSecUserPWHistoryService.findAll()\n");
+		}
+		else {
+			messages.append("Retrieved " + secUserPWHistoryResults.size() + " entities from CFSec.SecUserPWHistory\n");
 		}
 
 		List<?> secSysGrpResults = cFSecSecSysGrpService.findAll();
@@ -257,30 +281,6 @@ public class CFSecJpaTestTestSchema {
 		}
 		else {
 			messages.append("Retrieved " + secSessionResults.size() + " entities from CFSec.SecSession\n");
-		}
-
-		List<?> secUserResults = cFSecSecUserService.findAll();
-		if (secUserResults == null) {
-			messages.append("Erroneously retrieved null for CFSecSecUserService.findAll()\n");
-		}
-		else {
-			messages.append("Retrieved " + secUserResults.size() + " entities from CFSec.SecUser\n");
-		}
-
-		List<?> secUserPasswordResults = cFSecSecUserPasswordService.findAll();
-		if (secUserPasswordResults == null) {
-			messages.append("Erroneously retrieved null for CFSecSecUserPasswordService.findAll()\n");
-		}
-		else {
-			messages.append("Retrieved " + secUserPasswordResults.size() + " entities from CFSec.SecUserPassword\n");
-		}
-
-		List<?> secUserPWHistoryResults = cFSecSecUserPWHistoryService.findAll();
-		if (secUserPWHistoryResults == null) {
-			messages.append("Erroneously retrieved null for CFSecSecUserPWHistoryService.findAll()\n");
-		}
-		else {
-			messages.append("Retrieved " + secUserPWHistoryResults.size() + " entities from CFSec.SecUserPWHistory\n");
 		}
 
 		List<?> sysClusterResults = cFSecSysClusterService.findAll();
